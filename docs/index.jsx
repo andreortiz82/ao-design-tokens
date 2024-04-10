08860e558889013a9fb189e9bcf2707364407bfb
andreortiz82 <andreortiz82@gmail.com>
2024-04-10 18:01:12 -0500

    updating gallery and bump version

--- docs/index.jsx
+++ docs/index.jsx
@@ -8,11 +8,11 @@ const filterByMode = (data, mode) => {
 };
 
 const TokenTable = (props) => {
-  const { title, data, type } = props;
+  const { title, data, type, hash } = props;
 
   return (
-    <div className="TokenTable">
-      <h2>{title}</h2>
+    <article className="TokenTable">
+      <h2 id={hash}>{title}</h2>
       <table>
         <thead>
           <th>Token</th>
@@ -48,66 +48,41 @@ const TokenTable = (props) => {
               })}
         </tbody>
       </table>
-    </div>
+    </article>
   );
 };
 
 const Gallery = () => {
   return (
-    <div>
-      <h1>AO Design Tokens</h1>
-      <p>
-        A collection of design tokens for the AO design system. These tokens
-        support light and dark mode and are based on the{" "}
-        <a target="_blank" href="https://www.radix-ui.com/colors">
-          Radix Color System.
-        </a>
-        <ol>
-          <li>
-            To get started, install the package from npm:
-            <pre>
-              <code>npm install ao-design-tokens</code>
-            </pre>
-          </li>
-          <li>
-            Import the tokens in your project:
-            <pre>
-              <code>@import "ao-design-tokens";</code>
-            </pre>
-          </li>
-          <li>
-            Use the tokens in your CSS:
-            <pre>
-              <code>
-                .my-element &#123; <br />
-                &nbsp;&nbsp;background-color: var(--colors-pink-9); <br />
-                &nbsp;&nbsp;color: var(--color-primary); <br />
-                &#125;
-              </code>
-            </pre>
-          </li>
-        </ol>
-      </p>
-
+    <section className="Gallery">
+      <TokenTable
+        title="Semantic"
+        data={window.semantic}
+        type="semantic"
+        hash="semantic"
+      />
       <TokenTable
         title="Foundation Light"
         data={filterByMode(window.foundation, "light-")}
         type="foundation"
+        hash="light"
       />
       <TokenTable
         title="Foundation Dark"
         data={filterByMode(window.foundation, "dark-")}
         type="foundation"
+        hash="dark"
       />
-      <TokenTable title="Semantic" data={window.semantic} type="semantic" />
-      <TokenTable title="FX" data={window.fx} type="fx" />
-      <TokenTable title="Size" data={window.size} type="size" />
+
+      <TokenTable title="Size" data={window.size} type="size" hash="size" />
       <TokenTable
         title="Typography"
         data={window.typography}
         type="typography"
+        hash="typography"
       />
-    </div>
+      <TokenTable title="FX" data={window.fx} type="fx" hash="fx" />
+    </section>
   );
 };
 
