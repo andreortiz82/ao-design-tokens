7fe692307693ff8fd98d6d6d0232a4da4d211635
Andre Ortiz <andreortiz@Andres-Personal-M4.local>
2025-02-03 00:54:56 -0600

    udating build script

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "ao-design-tokens/",
   build: {
     outDir: "docs/",
   },
